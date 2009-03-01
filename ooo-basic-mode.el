3c661a45a29a9865a24bbb52149cd7940812d4ab
Takeshi Abe <tabe@fixedpoint.jp>
2009-03-01 18:33:38 +0900

    added keyword 'Call' and 'Set'.

--- ooo-basic-mode.el
+++ ooo-basic-mode.el
@@ -38,6 +38,7 @@
 (defvar ooo-basic-keywords
   '("As"
     "ByVal"
+    "Call"
     "Case"
     "Const"
     "Do"
@@ -64,6 +65,7 @@
     "Public"
     "Resume"
     "Select"
+    "Set"
     "Static"
     "Then"
     "To"

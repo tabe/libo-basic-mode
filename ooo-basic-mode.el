badf9fb1c89317bed1651c6e94526488c471487d
Takeshi Abe <tabe@fixedpoint.jp>
2009-05-06 14:03:05 +0900

    bump up version

--- ooo-basic-mode.el
+++ ooo-basic-mode.el
@@ -16,7 +16,7 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;; Author: Takeshi Abe <tabe@fixedpoint.jp>
-;; Version: 0.0.1
+;; Version: 0.0.2_alpha
 ;; Keywords: languages, basic, not so evil
 
 ;; Installation:
@@ -57,7 +57,7 @@ nil otherwise."
       (setq r (cons (list e i) r))
       (setq i (+ i 1)))))
 
-(defconst ooo-basic-mode-version "0.0.1"
+(defconst ooo-basic-mode-version "0.0.2_alpha"
   "Version string for ooo-basic-mode.")
 
 (defgroup ooo-basic nil

b2ccd8598bdc6d692cc63b0ea587839bcc5669c5
Takeshi Abe <tabe@fixedpoint.jp>
2009-05-29 00:29:48 +0900

    0.0.2 beta

--- ooo-basic-mode.el
+++ ooo-basic-mode.el
@@ -57,7 +57,7 @@ nil otherwise."
       (setq r (cons (list e i) r))
       (setq i (+ i 1)))))
 
-(defconst ooo-basic-mode-version "0.0.2_alpha"
+(defconst ooo-basic-mode-version "0.0.2_beta"
   "Version string for ooo-basic-mode.")
 
 (defgroup ooo-basic nil
